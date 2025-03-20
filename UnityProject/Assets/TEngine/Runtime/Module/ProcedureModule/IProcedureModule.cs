e405236d4dfbae020268c4391f00e82784f4d09e
Wkeasy <737814344@qq.com>
2025-03-20 14:54:15 +0800

    添加重启流程接口

--- UnityProject/Assets/TEngine/Runtime/Module/ProcedureModule/IProcedureModule.cs
+++ UnityProject/Assets/TEngine/Runtime/Module/ProcedureModule/IProcedureModule.cs
@@ -69,5 +69,14 @@ namespace TEngine
         /// <param name="procedureType">要获取的流程类型。</param>
         /// <returns>要获取的流程。</returns>
         ProcedureBase GetProcedure(Type procedureType);
+
+        /// <summary>
+        /// 重启流程。
+        /// <remarks>默认使用第一个流程作为启动流程。</remarks>
+        /// </summary>
+        /// <param name="procedures">新的的流程。</param>
+        /// <returns>是否重启成功。</returns>
+        /// <exception cref="GameFrameworkException">重启异常。</exception>
+        bool RestartProcedure(params ProcedureBase[] procedures);
     }
 }
