bc82685ec7903169314b9d17f033e0428ed00757
tpxxn <351765204@qq.com>
2025-05-09 13:51:47 +0800

    fix ExternalTypeUtil namespace

--- Configs/GameConfig/CustomTemplate/ExternalTypeUtil.cs
+++ Configs/GameConfig/CustomTemplate/ExternalTypeUtil.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public static class ExternalTypeUtil
 {
-    public static Vector2 NewVector2(cfg.vector2 v)
+    public static Vector2 NewVector2(GameConfig.vector2 v)
     {
         return new Vector2(v.X, v.Y);
     }
 
-    public static Vector3 NewVector3(cfg.vector3 v)
+    public static Vector3 NewVector3(GameConfig.vector3 v)
     {
         return new Vector3(v.X, v.Y, v.Z);
     }
 
-    public static Vector4 NewVector4(cfg.vector4 v)
+    public static Vector4 NewVector4(GameConfig.vector4 v)
     {
         return new Vector4(v.X, v.Y, v.Z, v.W);
     }
 
-    public static Vector2Int NewVector2Int(cfg.vector2int v)
+    public static Vector2Int NewVector2Int(GameConfig.vector2int v)
     {
         return new Vector2Int(v.X, v.Y);
     }
 
-    public static Vector3Int NewVector3Int(cfg.vector3int v)
+    public static Vector3Int NewVector3Int(GameConfig.vector3int v)
     {
         return new Vector3Int(v.X, v.Y, v.Z);
     }
