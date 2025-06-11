9aef4d4081a837bdf1ce9b6872faa1e70329b9dc
tpxxn <351765204@qq.com>
2025-06-11 11:16:25 +0800

    SetSpriteExtension 回调

--- UnityProject/Assets/TEngine/Runtime/Module/ResourceModule/Extension/Implement/SetSpriteExtensions.cs
+++ UnityProject/Assets/TEngine/Runtime/Module/ResourceModule/Extension/Implement/SetSpriteExtensions.cs
@@ -12,10 +12,11 @@ public static class SetSpriteExtensions
     /// <param name="image">UI/Image。</param>
     /// <param name="location">资源定位地址。</param>
     /// <param name="setNativeSize">是否使用原始分辨率。</param>
+    /// <param name="callback">设置完Sprite的回调</param>
     /// <param name="cancellationToken">取消设置资源的Token。</param>
-    public static void SetSprite(this Image image, string location, bool setNativeSize = false, CancellationToken cancellationToken = default)
+    public static void SetSprite(this Image image, string location, bool setNativeSize = false, Action<Image> callback = null, CancellationToken cancellationToken = default)
     {
-        ResourceExtComponent.Instance.SetAssetByResources<Sprite>(SetSpriteObject.Create(image, location, setNativeSize, cancellationToken)).Forget();
+        ResourceExtComponent.Instance.SetAssetByResources<Sprite>(SetSpriteObject.Create(image, location, setNativeSize, callback, cancellationToken)).Forget();
     }
 
     /// <summary>
@@ -23,12 +24,13 @@ public static class SetSpriteExtensions
     /// </summary>
     /// <param name="spriteRenderer">2D/SpriteRender。</param>
     /// <param name="location">资源定位地址。</param>
+    /// <param name="callback">设置完Sprite的回调</param>
     /// <param name="cancellationToken">取消设置资源的Token。</param>
-    public static void SetSprite(this SpriteRenderer spriteRenderer, string location, CancellationToken cancellationToken = default)
+    public static void SetSprite(this SpriteRenderer spriteRenderer, string location, Action<SpriteRenderer> callback = null, CancellationToken cancellationToken = default)
     {
-        ResourceExtComponent.Instance.SetAssetByResources<Sprite>(SetSpriteObject.Create(spriteRenderer, location, cancellationToken)).Forget();
+        ResourceExtComponent.Instance.SetAssetByResources<Sprite>(SetSpriteObject.Create(spriteRenderer, location, callback, cancellationToken)).Forget();
     }
-    
+
     /// <summary>
     /// 设置子图片。
     /// </summary>
